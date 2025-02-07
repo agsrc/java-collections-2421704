63a0f2931fdd07d65e14b6b32de72fb7540471cb
Akshay <34899682+agsrc@users.noreply.github.com>
2025-02-07 09:17:43 +0530

    solution

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -1,29 +1,27 @@
 package com.linkedin.collections;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RoomService {
 
-	private Collection<Room> inventory;
+	private final Collection<Room> inventory;
 
 	public RoomService() {
-		this.inventory = new HashSet<>();
+		this.inventory = new LinkedHashSet<>();
 	}
 	
 	public boolean hasRoom(Room room) {
 		
 	// 1. Returns a boolean that indicates if the Room Inventory contains a Room.
 		
-		return false;
+		return inventory.contains(room);
 	}
 	
 	public Room[] asArray() {
 		
 	// 2. Returns all Rooms as an Array of Rooms in the **order** they were Added.
-		
-		return null;
+		return inventory.toArray(new Room[0]);
 	}
 	
 	public Collection<Room> getByType(String type){
@@ -32,9 +30,10 @@ public class RoomService {
 	   3. Return a new Collection of Rooms where Room#type matches the provided String.
 		  The original Room Inventory collection MUST NOT BE MODIFIED.
 	*/
-		
-		return null;
-		
+		Collection<Room> roomByType = new ArrayList<>();
+		return inventory.stream()
+				.filter(room -> type.equals(room.getType()))
+				.collect(Collectors.toList());
 	}
 
 	public Collection<Room> getInventory() {
